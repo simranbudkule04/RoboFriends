c9732907a0d6abe9b23a535f429186ad0c5c5ff2
simranbudkule04 <113832457+simranbudkule04@users.noreply.github.com>
2023-08-07 22:37:16 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import 'tachyons';
+
+import App from './containers/App';
+import { requestRobots, searchRobots } from './reducers';
+
+import './index.css';
+
+const logger = createLogger();
+
+const rootReducers = combineReducers({ requestRobots, searchRobots });
+
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+} else {
+  console.error('Could not find root element to mount the application.');
+}
